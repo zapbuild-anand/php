6542a049b78963c7326bddef9bcf1c19e3c3fbbb
anand kumar <59948981+zapbuild-anand@users.noreply.github.com>
2020-01-20 13:21:38 +0530

    Add files via upload

---
+++ toString.php
@@ -0,0 +1,23 @@
+<?php
+//to string
+class myClass 
+{
+    function __construct() {
+    	$this->name="Anand";
+    	$this->id="za[321";
+        echo "In constructor"."<br>";
+    }
+
+    function __destruct() {
+        echo "<br>"."Destroying " . __CLASS__ . "<br>";
+    }
+    public function __toString()
+    {
+    	return $this->name;
+    }
+}
+
+$obj = new myClass();
+//calls toString function
+echo $obj;
+?>
