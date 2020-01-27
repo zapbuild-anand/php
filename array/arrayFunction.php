f3282ce6338f9e9fdda75ca6de109e35f49d754f
zapbuild-anand <anandkumar.zapbuild@gmail.com>
2020-01-27 18:07:22 +0530

    array functions

--- array/arrayFunction.php
+++ array/arrayFunction.php
@@ -16,6 +16,23 @@ foreach($arr as $x=>$value)
 //multidimentional array
 $arr=array(array("Name"=>"Anand","ID"=>"zap11"),array("Name"=>"Ankit","ID"=>"zap12"));
 print_r($arr);
-
-
+echo "<br>";
+$arrName=array("Anand","Pulok","Ankit","Shiv");
+$arrAge=array(23,24,23,24);
+//combine two array with first as key and second as value
+$arrCombine=array_combine($arrName,$arrAge);
+print_r($arrCombine);
+$arrName2=array("Deepak","Pulok");
+$arrMerge=array_merge($arrName,$arrName2);
+echo "<br>";
+//merge the data of two array
+print_r($arrMerge);
+//difference of two arrays
+$arrDiff=array_diff($arrName,$arrName2);
+echo "<br>";
+print_r($arrDiff);
+//fill array upto given number with value
+$arrNew=array_fill(3,4,"Hi");
+echo "<br>";
+print_r($arrNew);
 ?>
