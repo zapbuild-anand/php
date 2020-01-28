e3690ce0c2b2f239ed2c627d9ea8c3cb6011495f
zapbuild-anand <anandkumar.zapbuild@gmail.com>
2020-01-28 17:44:32 +0530

    associative array

---
+++ array/array_ex.php
@@ -0,0 +1,20 @@
+<?php
+$ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg",
+            "Belgium"=> "Brussels", "Denmark"=>"Copenhagen",
+            "Finland"=>"Helsinki", "France" => "Paris",
+            "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", 
+            "Germany" => "Berlin", "Greece" => "Athens", 
+            "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", 
+            "Portugal"=>"Lisbon", "Spain"=>"Madrid",
+            "Sweden"=>"Stockholm", "United Kingdom"=>"London",
+            "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", 
+            "Czech Republic"=>"Prague", "Estonia"=>"Tallin", 
+            "Hungary"=>"Budapest", "Latvia"=>"Riga", 
+            "Malta"=>"Valetta", "Austria" => "Vienna", 
+            "Poland"=>"Warsaw") ;
+foreach($ceu as $x=>$value)
+{
+    echo "The capital of $x is $value"."<br>";
+}
+
+?>
