03558f436d4731da57637f0467970ea95c07a30c
zapbuild-anand <anandkumar.zapbuild@gmail.com>
2020-01-28 17:50:17 +0530

    switch case

---
+++ basics/php_switch.php
@@ -0,0 +1,29 @@
+<?php
+$day=2;
+switch($day)
+{
+    case 1:
+        echo "Monday";
+    break;
+    case 2:
+        echo "Tuesday";
+    break;
+    case 3:
+        echo "Wednesday";
+    break;
+    case 4:
+        echo "Thursday";
+    break;
+    case 5:
+        echo "Friday";
+    break;
+    case 6:
+        echo "Satueday";
+    break;
+    case 7:
+        echo "Sunday";
+    break;
+    default:
+        echo "Invalid choice";
+}
+?>
