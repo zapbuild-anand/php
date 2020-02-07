c6bd8e2ce4e039f335a5bb7f6a4dfbcbd8e4269a
zapbuild-anand <anandkumar.zapbuild@gmail.com>
2020-02-07 17:18:48 +0530

    session use

--- sessionCookies/session.php
+++ sessionCookies/session.php
@@ -9,9 +9,16 @@ else
 	$_SESSION['counter']=1;
 }
 echo "Number of times you have visited this page :".$_SESSION['counter']."<br>";
-$_SESSION['Name']="Anand Kumar";
-$_SESSION['ID']="ZAP22093";
-echo "Session set";
+$_SESSION['user1']=array("Name"=>"Anand","ID"=>"Zap123");
+$_SESSION['user2']=array("Name"=>"Pulok","ID"=>"Zap124");
+echo "Session set"."<br>";
+echo "Session ID : ".session_id()."<br>";
+echo "User 1 details ";
+print_r($_SESSION['user1']);
+echo "<br>";
+echo "User 2 details ";
+print_r($_SESSION['user1']);
+
 //session_unset();
 //session_destroy();
 ?>
